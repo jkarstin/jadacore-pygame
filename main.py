7701363d893bf99d39f89c5c70575367488f6411
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2022-04-27 15:42:09 -0700

    Added test_main.py and resources directory; Added basic example code for pygame

--- main.py
+++ main.py
@@ -19,12 +19,28 @@ EXIT_SUCCESS = 0
 ### MAIN FUNCTION DEFINITION ###
 
 def main(argv: list[str], argc: int) -> int:
-    print('Hello world!')
-    
-    if argc > 1:
-        print(argv)
+    size = width, height = 320, 240
+    speed = [2, 2]
+    black = 0, 0, 0
 
-    return EXIT_SUCCESS
+    screen = pygame.display.set_mode(size)
+
+    ball = pygame.image.load("resources/intro_ball.gif")
+    ballrect = ball.get_rect()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: return EXIT_SUCCESS
+        
+        ballrect = ballrect.move(0.25, 0.25)
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > height:
+            speed[1] = -speed[1]
+        
+        screen.fill(black)
+        screen.blit(ball, ballrect)
+        pygame.display.flip()
 
 
 ### CORE FUNCTIONS ###
