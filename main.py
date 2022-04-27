ca25144c13f0535d37eec76a8e3a3ff947feae01
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2022-04-27 12:47:57 -0700

    Set up starting template for main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import sys
+#################################
+# main.py
+#-------------------------------
+# J Karstin Neill    MM.DD.YYYY
+#################################
 
+
+### IMPORTS ###
+
+import sys, pygame
+
+
+### MAIN FUNCTION DEFINITION ###
 
 def main(argv):
     print('Hello world!')
@@ -7,5 +18,18 @@ def main(argv):
     return 0
 
 
+### CORE FUNCTIONS ###
+
+
+
+
+### UTILITY FUNCTIONS ###
+
+
+
+
+### MAIN FUNCTION EXECUTION ###
+
 if __name__ == '__main__':
+    pygame.init()
     sys.exit(main(sys.argv))
